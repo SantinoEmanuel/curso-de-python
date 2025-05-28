b98bb5adeb6f96a978d940eb376c4437e73d0732
SantinoEmanuel <morenosantino2510@gmail.com>
2025-05-28 18:02:06 -0300

    clase 1- Varialbles

---
+++ variables.py
@@ -0,0 +1,10 @@
+numeros = int(input("ingrese un nùmeros: "))
+print(numeros)
+#numero enteros
+numl = 25
+#numero flotantes
+floi = 25,9
+#string
+cadena = "hola Python"
+#booleanos
+booleano = True
